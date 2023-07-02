071942d7b2d977fb4b6b0f239a243b2f402f0e1b
abdulkerim <abdulkerimcann@outlook.com>
2023-07-02 16:46:27 +0300

    random header has been added

--- Netflix Clone/Controllers/Core/HomeViewController.swift
+++ Netflix Clone/Controllers/Core/HomeViewController.swift
@@ -18,6 +18,8 @@ enum Sections: Int {
 class HomeViewController: UIViewController {
     
     let sectionHeader: [String] = ["Trending Movies","Trending TV","Popular","Upcoming Movies","Top Rated"]
+    private var randomTrendingMovie: Title?
+    private var headerView: HeroHeaderUIView?
     
     private let homeFeedTable: UITableView = {
         let table = UITableView(frame: .zero,style: .grouped)
@@ -34,9 +36,23 @@ class HomeViewController: UIViewController {
         
         configureNavbar()
         
-        let tableHeaderView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 500))
-        homeFeedTable.tableHeaderView = tableHeaderView
+        headerView  = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 500))
+        homeFeedTable.tableHeaderView = headerView
+        configureHeroHeaderView()
         
+    }
+    
+    private func configureHeroHeaderView() {
+        APICaller.shared.getTrendingMovies { result in
+            switch result {
+            case .success(let titles):
+                let selectedTitle = titles.randomElement()
+                self.randomTrendingMovie = selectedTitle
+                self.headerView?.configure(with: TitleViewModel(titleName: selectedTitle?.original_title ?? "", posterURL: selectedTitle?.poster_path ?? ""))
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
     private func configureNavbar() {
